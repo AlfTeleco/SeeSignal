86844459e87c4dded6a0d4ec1c09f00bbcccce9e
AlvaroSegula <aguzman@segula.es>
2020-06-19 07:56:13 +0200

    Adding lines to ensure self-content installer

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QCoreApplication::addLibraryPath(QDir::currentPath());
+    QCoreApplication::addLibraryPath(QDir::currentPath()+"/platforms");
     MainWindow w;
     w.show();
     return a.exec();
